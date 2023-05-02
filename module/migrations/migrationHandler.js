25981077e113db666da6abdf4a224c836d039d97
Mike <48950375+Mikerspp@users.noreply.github.com>
2023-05-02 12:03:30 +0100

    delet

--- module/migrations/migrationHandler.js
+++
@@ -1,7 +0,0 @@
-import migration_1_1_0 from './migration_1_1_0.js';
-import migration_1_4_0 from './migration_1_4_0.js';
-
-export default async function processMigrations() {
-    await migration_1_1_0.processMigration();
-    await migration_1_4_0.processMigration();
-}
